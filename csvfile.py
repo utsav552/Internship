0a20643ad3836d5f8356f23d88a3bbe14672a6aa
Utsav Purohit <utsavprht79@gmail.com>
2025-11-18 12:10:51 +0530

    acsvfile.py

--- csvfile.py
+++ csvfile.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime , timedelta
 import csv
-
+import random
 
 start_time= datetime(year =2025 , month = 11, day = 1, hour = 1, minute= 0)
 
@@ -13,10 +13,15 @@ for i in range(60):
     current_time = start_time + timedelta(minutes=i)
     epoch_time = current_time.timestamp()
     power=100+i
+
+    if random.random() < 0.25:
+        power="N/A"
+        
     rec.append([epoch_time, power])
 
 with open("a.csv" , mode= 'w' , newline= '') as file:
     writer = csv.writer(file)
     writer.writerows(rec)
+
 
 print ( "csv file is created")
