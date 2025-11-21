87001ca579449726a0ad3f436b836132114ea87e
Utsav Purohit <utsavprht79@gmail.com>
2025-11-21 12:20:32 +0530

    mergecsv.py

--- mergecsv.py
+++ mergecsv.py
@@ -14,24 +14,25 @@ p = next(power, None)
 e = next(energy, None)
 
 with open("merged.csv", "w", newline="") as fm:
-    w = csv.writer(fm)
-    w.writerow(["epoch_time", "gmt_dt", "Power", "Energy"])
+    writer = csv.writer(fm)
+    writer.writerow(["epoch_time", "gmt_dt", "Power", "Energy"])
 
     while p or e:
 
         if e is None or(p and p[0] < e[0]):
             
-            w.writerow([p[0], p[1], p[2], "N/A"])
+            writer.writerow([p[0], p[1], p[2], "N/A"])
             p = next(power, None)
 
         elif p is None or (e and e[0] < p[0]):
             
-            w.writerow([e[0], e[1], "N/A", e[2]])
+            writer.writerow([e[0], e[1], "N/A", e[2]])
             e = next(energy, None)
 
         else:
-            w.writerow([p[0], p[1], p[2], e[2]])
+            writer.writerow([p[0], p[1], p[2], e[2]])
             p = next(power, None)
             e = next(energy, None)
 
 print("merged.csv created successfully!")
+
