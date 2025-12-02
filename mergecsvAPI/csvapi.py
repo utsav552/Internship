552cc2a2c6cc30b32eb047b89b4d746fd48079b0
Utsav Purohit <utsavprht79@gmail.com>
2025-12-02 14:07:26 +0530

    csvapi.py

--- mergecsvAPI/csvapi.py
+++ mergecsvAPI/csvapi.py
@@ -1,30 +1,64 @@
 from fastapi import FastAPI, UploadFile, File
-import pandas as pd
 from fastapi.responses import StreamingResponse
+import csv
 import io
+import random
 
 app = FastAPI()
 
 @app.post("/merge-csv/")
 async def merge_csv(
-    file1: UploadFile = File(...), 
+    file1: UploadFile = File(...),
     file2: UploadFile = File(...)
-    ):
+):
+    f1_text = await file1.read()
+    f1_stream = io.StringIO(f1_text.decode())
+    reader1 = csv.DictReader(f1_stream)
 
-    df1 = pd.read_csv(file1.file)
-    df2 = pd.read_csv(file2.file)
+    data1 = {}
+    for row in reader1:
+        key = (row["epoch_time"], row["gmt_datetime"])
+        data1[key] = {
+            "epoch_time": row["epoch_time"],
+            "gmt_datetime": row["gmt_datetime"],
+            "Energy": row.get("Energy", "N/A")
+        }
 
-    merged_df = pd.merge(df1, df2, on=["epoch_time", "gmt_datetime"], how="outer").fillna("N/a")
+    f2_text = await file2.read()
+    f2_stream = io.StringIO(f2_text.decode())
+    reader2 = csv.DictReader(f2_stream)
 
-    stream = io.StringIO()
-    merged_df.to_csv(stream, index=False)
-    stream.seek(0)
+    data2 = {}
+    for row in reader2:
+        key = (row["epoch_time"], row["gmt_datetime"])
+        data2[key] = {"Power": row.get("Power", "N/A")}
+
+    all_keys = set(data1.keys()) | set(data2.keys())
+    merged_rows = []
+
+    for key in sorted(all_keys):
+        epoch, gmt = key
+
+        energy = data1.get(key, {}).get("Energy", "N/A")
+        power = data2.get(key, {}).get("Power", "N/A")
+
+        merged_rows.append({
+            "epoch_time": epoch,
+            "gmt_datetime": gmt,
+            "Energy": energy,
+            "Power": power
+        })
+
+    random.shuffle(merged_rows)
+
+    output_stream = io.StringIO()
+    writer = csv.DictWriter(output_stream, fieldnames=["epoch_time", "gmt_datetime", "Energy", "Power"])
+    writer.writeheader()
+    writer.writerows(merged_rows)
+    output_stream.seek(0)
 
     return StreamingResponse(
-        iter([stream.getvalue()]),
+        iter([output_stream.getvalue()]),
         media_type="text/csv",
-        headers={
-            "Content-Disposition": "attachment; filename=merged.csv"
-        }
+        headers={"Content-Disposition": "attachment; filename=merged.csv"}
     )
-
