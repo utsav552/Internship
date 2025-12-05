90aa0893f24b3acbd7eb7fff7291748ca48b9c17
Utsav Purohit <utsavprht79@gmail.com>
2025-12-05 18:26:17 +0530

    Update csvapi.py

--- mergecsvAPI/csvapi.py
+++ mergecsvAPI/csvapi.py
@@ -2,64 +2,88 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
 import csv
 import io
-import random
 
 app = FastAPI()
+
+def row_generator(file1, file2):
+    reader1 = csv.DictReader(io.TextIOWrapper(file1, encoding="utf-8"))
+    reader2 = csv.DictReader(io.TextIOWrapper(file2, encoding="utf-8"))
+
+    row1 = next(reader1, None)
+    row2 = next(reader2, None)
+
+    while row1 or row2:
+        if row1 and row2:
+            key1 = (row1["epoch_time"], row1["gmt_datetime"])
+            key2 = (row2["epoch_time"], row2["gmt_datetime"])
+
+            if key1 == key2:
+                yield {
+                    "epoch_time": row1["epoch_time"],
+                    "gmt_datetime": row1["gmt_datetime"],
+                    "Energy": row1.get("Energy", "N/A"),
+                    "Power": row2.get("Power", "N/A"),
+                }
+                row1 = next(reader1, None)
+                row2 = next(reader2, None)
+
+            elif key1 < key2:
+                yield {
+                    "epoch_time": row1["epoch_time"],
+                    "gmt_datetime": row1["gmt_datetime"],
+                    "Energy": row1.get("Energy", "N/A"),
+                    "Power": "N/A"
+                }
+                row1 = next(reader1, None)
+
+            else:
+                yield {
+                    "epoch_time": row2["epoch_time"],
+                    "gmt_datetime": row2["gmt_datetime"],
+                    "Energy": "N/A",
+                    "Power": row2.get("Power", "N/A")
+                }
+                row2 = next(reader2, None)
+
+        elif row1:
+            yield {
+                "epoch_time": row1["epoch_time"],
+                "gmt_datetime": row1["gmt_datetime"],
+                "Energy": row1.get("Energy", "N/A"),
+                "Power": "N/A"
+            }
+            row1 = next(reader1, None)
+
+        elif row2:
+            yield {
+                "epoch_time": row2["epoch_time"],
+                "gmt_datetime": row2["gmt_datetime"],
+                "Energy": "N/A",
+                "Power": row2.get("Power", "N/A")
+            }
+            row2 = next(reader2, None)
 
 @app.post("/merge-csv/")
 async def merge_csv(
     file1: UploadFile = File(...),
     file2: UploadFile = File(...)
 ):
-    f1_text = await file1.read()
-    f1_stream = io.StringIO(f1_text.decode())
-    reader1 = csv.DictReader(f1_stream)
+    def generate():
+        output = io.StringIO()
+        writer = csv.DictWriter(output, fieldnames=["epoch_time", "gmt_datetime", "Energy", "Power"])
+        writer.writeheader()
+        yield output.getvalue()
+        output.seek(0)
+        output.truncate(0)
 
-    data1 = {}
-    for row in reader1:
-        key = (row["epoch_time"], row["gmt_datetime"])
-        data1[key] = {
-            "epoch_time": row["epoch_time"],
-            "gmt_datetime": row["gmt_datetime"],
-            "Energy": row.get("Energy", "N/A")
-        }
-
-    f2_text = await file2.read()
-    f2_stream = io.StringIO(f2_text.decode())
-    reader2 = csv.DictReader(f2_stream)
-
-    data2 = {}
-    for row in reader2:
-        key = (row["epoch_time"], row["gmt_datetime"])
-        data2[key] = {"Power": row.get("Power", "N/A")}
-
-    all_keys = set(data1.keys()) | set(data2.keys())
-    merged_rows = []
-
-    for key in sorted(all_keys):
-        epoch, gmt = key
-
-        energy = data1.get(key, {}).get("Energy", "N/A")
-        power = data2.get(key, {}).get("Power", "N/A")
-
-        merged_rows.append({
-            "epoch_time": epoch,
-            "gmt_datetime": gmt,
-            "Energy": energy,
-            "Power": power
-        })
-
-    random.shuffle(merged_rows)
-
-    output_stream = io.StringIO()
-    writer = csv.DictWriter(output_stream, fieldnames=["epoch_time", "gmt_datetime", "Energy", "Power"])
-    writer.writeheader()
-    writer.writerows(merged_rows)
-    output_stream.seek(0)
+        for row in row_generator(file1.file, file2.file):
+            writer.writerow(row)
+            yield output.getvalue()
+            output.seek(0)
+            output.truncate(0)
 
     return StreamingResponse(
-        iter([output_stream.getvalue()]),
+        generate(),
         media_type="text/csv",
         headers={"Content-Disposition": "attachment; filename=merged.csv"}
     )
-
